da49cc6a6572370dc7054e6f17ffb9cdd1124c64
SwethaChavalla <schavalla@hrgdev.com>
2017-02-13 15:51:59 +0400

    Added passing inputs from outside.

---
+++ cokeProject/src/dataTesting/PopprobeComparingCountryLevelData.java
@@ -0,0 +1,35 @@
+package dataTesting;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import jxl.read.biff.BiffException;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
+
+public class PopprobeComparingCountryLevelData {
+
+	public static void main(String[] args)
+			throws InterruptedException, RowsExceededException, BiffException, WriteException, IOException {
+		WebDriver driver = new FirefoxDriver();
+		PopprobeLogin login = new PopprobeLogin();
+		PopprobeComparingCountryLevelDataWithAndWithOutCooler cooler = new PopprobeComparingCountryLevelDataWithAndWithOutCooler();
+		String date = "2016 - 12";
+		String country = "BAHAMAS";
+		String channel = "HOME MARKET TRADITIONAL";
+		String readFilePath = "C:/Users/Mona Lisa/Desktop/Datafor countrylevel testing.xls" ;
+		String writeFilePath = "C:/Users/Mona Lisa/Downloads/country data.xls"; 
+		login.logIn(driver);
+		Thread.sleep(8000);
+		login.selectDropDowns(driver, date, country, channel);
+		cooler.compareCountryLevelCoolerData(driver,readFilePath,writeFilePath);
+		PopprobeComparingCountryLevelDataWithCooler yes = new PopprobeComparingCountryLevelDataWithCooler();
+		yes.comparingCountryLevelDataWithCooler(driver,readFilePath,writeFilePath);
+		PopprobeComparingCountryLevelDataWithOutCooler no = new PopprobeComparingCountryLevelDataWithOutCooler();
+		no.comparingCountryLevelDataWithOutCooler(driver,readFilePath,writeFilePath);
+	}
+
+}
