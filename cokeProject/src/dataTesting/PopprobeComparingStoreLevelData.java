d51ec82437b26fde108d6863b795d542014b10e1
SwethaChavalla <schavalla@hrgdev.com>
2017-02-14 13:28:26 +0400

    Comparing data method is kept in another class.

--- cokeProject/src/dataTesting/PopprobeComparingStoreLevelData.java
+++ cokeProject/src/dataTesting/PopprobeComparingStoreLevelData.java
@@ -1,20 +1,12 @@
 package dataTesting;
 
-import java.util.List;
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import java.io.File;
-import java.io.FileOutputStream;
+
 import java.io.IOException;
-import jxl.Cell;
-import jxl.Sheet;
-import jxl.Workbook;
+
 import jxl.read.biff.BiffException;
-import jxl.write.Label;
-import jxl.write.WritableSheet;
-import jxl.write.WritableWorkbook;
 import jxl.write.WriteException;
 
 public class PopprobeComparingStoreLevelData {
@@ -30,14 +22,15 @@ public class PopprobeComparingStoreLevelData {
 		PopprobeLogin login = new PopprobeLogin();
 		login.logIn(driver);
 		login.selectDropDowns(driver, date, country, channel);
+		ComparingStoreLevelDataAndWritingXL  compare = new ComparingStoreLevelDataAndWritingXL();
 		ReadingdatafromUI namesAndTotal = new ReadingdatafromUI();
 		ReadingdatafromUI dataUI =namesAndTotal.readingDataFromUI(driver);
 		ReadingDataFromxl storeAndIce = new ReadingDataFromxl();
 	     ReadingDataFromxl xldata =	storeAndIce.readingDataFromXL(readFilePath);
-		login.comparingAndWritingData(writeFilePath,dataUI.getrowsCount(), dataUI.getTotalstringUI(), xldata.getRowsXL(), dataUI.getnamesUI(), xldata.getStoreNameXL(), xldata.getIceXL(),
+		compare.comparingAndWritingData(writeFilePath,dataUI.getrowsCount(), dataUI.getTotalstringUI(), xldata.getRowsXL(), dataUI.getnamesUI(), xldata.getStoreNameXL(), xldata.getIceXL(),
 				xldata.getCounTryXL(), xldata.getDaTeXL(),dataUI.getnamesUI(),xldata.getChaNnelXL(), xldata.getSubChanneLXL(),xldata.getCooLerXL(), 
 				dataUI.getTotalUI(), xldata.getIcevaLueXL());
-		System.out.println("For testing");
+		
 
 	}
 
