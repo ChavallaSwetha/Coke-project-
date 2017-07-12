c4024012b5ca3f758b3e74b3bdea472edcc7ff27
SwethaChavalla <schavalla@hrgdev.com>
2017-07-12 12:33:32 +0400

    Pushing latest code.

--- cokeProject/src/dataTesting/PopprobeComparingCountryLevelData.java
+++ cokeProject/src/dataTesting/PopprobeComparingCountryLevelData.java
@@ -1,38 +1,65 @@
 package dataTesting;
 
+import java.awt.AWTException;
 import java.io.IOException;
 
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import jxl.read.biff.BiffException;
 import jxl.write.WriteException;
-import jxl.write.biff.RowsExceededException;
-import popProbeRelatedPrograms.FilterBasedOnCriteria;
 
-public class PopprobeComparingCountryLevelData {
-
+public class tesTing {
 	public static void main(String[] args)
-			throws InterruptedException, RowsExceededException, BiffException, WriteException, IOException {
-		WebDriver driver = new FirefoxDriver();
-		PopprobeLogin login = new PopprobeLogin();
-		PopprobeComparingCountryLevelDataWithAndWithOutCooler cooler = new PopprobeComparingCountryLevelDataWithAndWithOutCooler();
-		String date = "2016 - 12";
+			throws InterruptedException, BiffException, IOException, WriteException, AWTException {
+		
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Users/Mona Lisa/Downloads/chromedriver_win32/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		String readFilePath = "C:/Users/Mona Lisa/Downloads/Caribbean ICE Results February 2017.xls";
 		String country = "BAHAMAS";
-		String channel = "HOME MARKET TRADITIONAL";
-		String readFilePath = "C:/Users/Mona Lisa/Desktop/Datafor countrylevel testing.xls" ;
-		String writeFilePath = "C:/Users/Mona Lisa/Downloads/country data.xls"; 
+		String compareCountry = "Bahamas";
+		String channelUI = "HOME MARKET TRADITIONAL";
+		String channelXL = "Tradicional";
+		String cooler = "1";
+		String coolerYes = "2";
+		
+		String piD3 = "3";
+		String writeFilePath = "C:/Users/Mona Lisa/Downloads/Reading file.xls ";
+		String date = "2017 - 2";
+		
+		
+		
+		WithAndWithOutCooler test = new WithAndWithOutCooler();
+		WithCooler yes = new WithCooler();
+		WithOutCooler no = new WithOutCooler();
+		
+		PopprobeLogin login = new PopprobeLogin();
 		login.logIn(driver);
-		Thread.sleep(8000);
-		FilterBasedOnCriteria filter = new FilterBasedOnCriteria();
-		filter.selectDropDowns(driver, date, country, channel);
-		cooler.compareCountryLevelCoolerData(driver,readFilePath,writeFilePath);
-		PopprobeComparingCountryLevelDataWithCooler yes = new PopprobeComparingCountryLevelDataWithCooler();
-		yes.comparingCountryLevelDataWithCooler(driver,readFilePath,writeFilePath);
-		PopprobeComparingCountryLevelDataWithOutCooler no = new PopprobeComparingCountryLevelDataWithOutCooler();
-		no.comparingCountryLevelDataWithOutCooler(driver,readFilePath,writeFilePath);
-		System.out.println("Comparing country level data");
-	}
+		
+		login.selectDropDowns(driver, date, country, channelUI);
+		ReadingCountryLevelDataFromUI dashboardData = new ReadingCountryLevelDataFromUI();
+		UIAndXLCountryLevelData uidata = dashboardData.readingDashBoardData(driver);
+		ReadingCountryLevelXLData dataFromXL = new ReadingCountryLevelXLData();
+		UIAndXLCountryLevelData xldata = dataFromXL.readingCountryLevelXLData(readFilePath, compareCountry, channelXL, cooler);
+		
+		
+		test.compareCountryLevelCoolerData(driver, readFilePath, date, writeFilePath, uidata, xldata);
+		
+		login.coolerDropDown(driver);
+		ReadingCountryLevelDataFromUI dashboardDataYes = new ReadingCountryLevelDataFromUI();
+		UIAndXLCountryLevelData uidataYes = dashboardDataYes.readingDashBoardData(driver);
+		ReadingCountryLevelXLData dataFromXLYes = new ReadingCountryLevelXLData();
+		UIAndXLCountryLevelData xldataYes = dataFromXLYes.readingCountryLevelXLData(readFilePath, compareCountry, channelXL, coolerYes);
+		        		
+		yes.compareCountryLevelCoolerData(driver, readFilePath,  date, writeFilePath, uidataYes, xldataYes);
+		
+		/*login.coolerDropDownNo(driver);
+		ReadingCountryLevelDataFromUI dashboardData2 = new ReadingCountryLevelDataFromUI();
+		UICountryLevelData data2 = dashboardData2.readingDashBoardData(driver);
 
+		no.compareCountryLevelCoolerData(driver, readFilePath, compareCountry, channelXL, piD3, writeFilePath,data2);*/
+		
+	
+}
 }
---
+++ cokeProject/src/popProbeRelatedPrograms/PopprobeComparingCountryLevelDat.java
@@ -0,0 +1,38 @@
+package dataTesting;
+
+import java.io.IOException;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import jxl.read.biff.BiffException;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
+import popProbeRelatedPrograms.FilterBasedOnCriteria;
+
+public class PopprobeComparingCountryLevelData {
+
+	public static void main(String[] args)
+			throws InterruptedException, RowsExceededException, BiffException, WriteException, IOException {
+		WebDriver driver = new FirefoxDriver();
+		PopprobeLogin login = new PopprobeLogin();
+		PopprobeComparingCountryLevelDataWithAndWithOutCooler cooler = new PopprobeComparingCountryLevelDataWithAndWithOutCooler();
+		String date = "2016 - 12";
+		String country = "BAHAMAS";
+		String channel = "HOME MARKET TRADITIONAL";
+		String readFilePath = "C:/Users/Mona Lisa/Desktop/Datafor countrylevel testing.xls" ;
+		String writeFilePath = "C:/Users/Mona Lisa/Downloads/country data.xls"; 
+		login.logIn(driver);
+		Thread.sleep(8000);
+		FilterBasedOnCriteria filter = new FilterBasedOnCriteria();
+		filter.selectDropDowns(driver, date, country, channel);
+		cooler.compareCountryLevelCoolerData(driver,readFilePath,writeFilePath);
+		PopprobeComparingCountryLevelDataWithCooler yes = new PopprobeComparingCountryLevelDataWithCooler();
+		yes.comparingCountryLevelDataWithCooler(driver,readFilePath,writeFilePath);
+		PopprobeComparingCountryLevelDataWithOutCooler no = new PopprobeComparingCountryLevelDataWithOutCooler();
+		no.comparingCountryLevelDataWithOutCooler(driver,readFilePath,writeFilePath);
+		System.out.println("Comparing country level data");
+	}
+
+}
